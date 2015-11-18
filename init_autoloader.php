a6bfab868c6330d10480fd6b2135021a482eb5c6
vikrantghan <vikrantghan@gmail.com>
2015-11-18 13:03:20 +0530

    cc
    c

--- init_autoloader.php
+++ init_autoloader.php
@@ -46,7 +46,7 @@ if ($zf2Path) {
         ));
     }
 }
-
+/* test vikrant */
 if (!class_exists('Zend\Loader\AutoloaderFactory')) {
     throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
 }
